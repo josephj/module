b4987c2255867968f1807c7051f9e2ba843eeca3
josephj <josephj6802@gmail.com>
2013-01-13 22:29:09 +0800

    Add support files.

---
+++ lang/module_en-US.js
@@ -0,0 +1,9 @@
+YUI.add("lang/module_en-US", function (Y) {
+    Y.Intl.add("module", "en-US", {
+        "alert_button": "OK",
+        "alert_default_title": "Notice",
+        "confirm_cancel_button": "Cancel",
+        "confirm_default_title": "Confirm?",
+        "confirm_ok_button": "OK"
+    });
+});
---
+++ lang/module_zh-TW.js
@@ -0,0 +1,9 @@
+YUI.add("lang/module_zh-TW", function (Y) {
+    Y.Intl.add("module", "zh-TW", {
+        "alert_button": "確定",
+        "alert_default_title": "注意！",
+        "confirm_cancel_button": "取消",
+        "confirm_default_title": "請確認？",
+        "confirm_ok_button": "確定"
+    });
+});
