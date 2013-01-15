ff9d48b77877239cec127ea311b4f7fb7062f220
josephj <josephj6802@gmail.com>
2013-01-15 12:09:49 +0800

    Fix after reviewing with miiiCasa UED.

--- lang/module_en-US.js
+++ lang/module_en-US.js
@@ -3,7 +3,7 @@ YUI.add("lang/module_en-US", function (Y) {
         "alert_button": "OK",
         "alert_default_title": "Notice",
         "confirm_cancel_button": "Cancel",
-        "confirm_default_title": "Confirm?",
+        "confirm_default_title": "Please Confirm",
         "confirm_ok_button": "OK"
     });
 });
